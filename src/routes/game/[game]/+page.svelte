2204c9bf0167c7749eaec166bd4dcd3d760536cd
Daniel Kim <chipperday@gmail.com>
2025-10-25 14:19:26 -0700

    class demo

--- src/routes/game/[game]/+page.svelte
+++ src/routes/game/[game]/+page.svelte
@@ -1,6 +1,6 @@
 <script>
   export let data;
-  import { onMount } from 'svelte';
+  import { onMount } from "svelte";
 
   let roster = []; // array of teams
 
@@ -13,46 +13,62 @@
     loadTeams();
   });
 
-	function deleteTeam(index) {
-		roster.splice(index, 1); 
-		roster = [...roster];    
-		localStorage.setItem("all-teams", JSON.stringify(roster));
-	}
+  function deleteTeam(index) {
+    roster.splice(index, 1);
+    roster = [...roster];
+    localStorage.setItem("all-teams", JSON.stringify(roster));
+  }
 
-	function toTitleCase(str) {
+  function toTitleCase(str) {
     return str
-      .replace(/-/g, ' ')                
-      .split(' ')                         
-      .map(word => word.charAt(0).toUpperCase() + word.slice(1)) 
-      .join(' ');
+      .replace(/-/g, " ")
+      .split(" ")
+      .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+      .join(" ");
   }
 </script>
 
 <div class="container mx-auto">
-	<h1 class="text-2xl">Pokémon {toTitleCase(data.game)}</h1>
+  <h1 class="text-2xl">Pokémon {toTitleCase(data.game)}</h1>
 
-	<a href={`/game/${data.game}/new-team`} class="btn btn-primary mb-4">Create a new team</a>
+  <a href={`/game/${data.game}/new-team`} class="btn btn-primary mb-4"
+    >Create a new team</a
+  >
 
-	<h2 class="mt-4 mb-2 text-xl text-center">List of Teams</h2>
+  <h2 class="mt-4 mb-2 text-xl text-center">List of Teams</h2>
 
-	<div class="flex flex-col gap-6">
-		{#each roster as team, ti}
-			<div class="border p-2 rounded">
-				<div class="flex justify-between items-center mb-2">
-					<h3 class="font-bold">Team {ti + 1}</h3>
-					<button class="btn btn-error btn-sm" onclick={() => deleteTeam(ti)}>Delete</button>
-				</div>
-				<div class="flex flex-wrap gap-2">
-					{#each team as pokemon}
-						<div class="w-20 card shadow-sm flex flex-col items-center justify-center p-1 bg-gray-100">
-							<figure>
-								<img src={pokemon.sprite} alt={toTitleCase(pokemon.name)} class="max-h-16 object-contain"/>
-							</figure>
-							<div class="text-sm text-center">{toTitleCase(pokemon.name)}</div>
-						</div>
-					{/each}
-				</div>
-			</div>
-		{/each}
-	</div>
+  <div class="flex flex-col gap-6">
+    {#if roster.length === 0}
+      You dont have any teams yet.
+      <a href={`/game/${data.game}/new-team`} class="btn btn-primary mb-4"
+        >Create a new team</a
+      >
+    {/if}
+    {#each roster as team, ti}
+      <div class="border p-2 rounded">
+        <div class="flex justify-between items-center mb-2">
+          <h3 class="font-bold">Team {ti + 1}</h3>
+          <button class="btn btn-error btn-sm" onclick={() => deleteTeam(ti)}
+            >Delete</button
+          >
+        </div>
+        <div class="flex flex-wrap gap-2">
+          {#each team as pokemon}
+            <div
+              class="w-20 card shadow-sm flex flex-col items-center justify-center p-1 bg-gray-100"
+            >
+              <figure>
+                <img
+                  src={pokemon.sprite}
+                  alt={toTitleCase(pokemon.name)}
+                  class="max-h-16 object-contain"
+                />
+              </figure>
+              <div class="text-sm text-center">{toTitleCase(pokemon.name)}</div>
+            </div>
+          {/each}
+        </div>
+      </div>
+    {/each}
+  </div>
 </div>
--- src/routes/game/[game]/new-team/+page.svelte
+++ src/routes/game/[game]/new-team/+page.svelte
@@ -1,43 +1,43 @@
 <script>
-  import pokemons from '../../../../pokemon.json';
-	import { goto } from '$app/navigation'
+  import pokemons from "../../../../pokemon.json";
+  import { goto } from "$app/navigation";
 
-	let { data } = $props();
-	let page = $state(1);
+  let { data } = $props();
+  let page = $state(1);
   let { game } = data;
-  let pokemon = $state(pokemons.filter((v) => v.versions.includes(game)))
-	
-	let selectedPokemon = $state(undefined);
-	let selected = $state([]);
+  let pokemon = $state(pokemons.filter((v) => v.versions.includes(game)));
 
-	$effect(() => {
-		console.log(selectedPokemon);
-	})
+  let selectedPokemon = $state(undefined);
+  let selected = $state([]);
 
-	function handleClick(index) {
-		if (!selected.includes(index)) {
-			if (selected.length < 6) {
-				selected.push(index);
-				selectedPokemon = pokemon[index];
-			}
-		} else {
-			selected = selected.filter(v => v !== index);
-		}
-	}
+  $effect(() => {
+    console.log(selectedPokemon);
+  });
 
-	function save() {
-		let team = pokemon.filter((_, i) => selected.includes(i));
+  function handleClick(index) {
+    if (!selected.includes(index)) {
+      if (selected.length < 6) {
+        selected.push(index);
+        selectedPokemon = pokemon[index];
+      }
+    } else {
+      selected = selected.filter((v) => v !== index);
+    }
+  }
 
-		let allTeams = JSON.parse(localStorage.getItem("all-teams")) || [];
+  function save() {
+    let team = pokemon.filter((_, i) => selected.includes(i));
 
-		allTeams.push(team);
+    let allTeams = JSON.parse(localStorage.getItem("all-teams")) || [];
 
-		localStorage.setItem("all-teams", JSON.stringify(allTeams));
+    allTeams.push(team);
 
-		goto(`/game/${game}`);
-	}
+    localStorage.setItem("all-teams", JSON.stringify(allTeams));
 
-	const perPage = 30; 
+    goto(`/game/${game}`);
+  }
+
+  const perPage = 30;
 
   function nextPage() {
     if (page * perPage < pokemon.length) page += 1;
@@ -47,90 +47,106 @@
     if (page > 1) page -= 1;
   }
 
-	function toTitleCase(str) {
+  function toTitleCase(str) {
     return str
-      .replace(/-/g, ' ')                
-      .split(' ')                         
-      .map(word => word.charAt(0).toUpperCase() + word.slice(1)) 
-      .join(' ');
+      .replace(/-/g, " ")
+      .split(" ")
+      .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+      .join(" ");
   }
 </script>
 
 <div class="flex flex-wrap">
-	<div class="w-screen flex justify-center">
-		<div class="grid grid-cols-6 gap-2 w-full max-w-5xl">
-			{#each Array(6) as _, i}
-				<div class="aspect-square w-full card shadow-sm flex items-center justify-center bg-red-200">
-					{#if selected[i] !== undefined}
-						<img src={pokemon[selected[i]].sprite} alt={pokemon[selected[i]].name} class="max-h-full max-w-full object-contain"/>
-					{/if}
-				</div>
-			{/each}
-		</div>
-	</div>
+  <div class="w-screen flex justify-center">
+    <div class="grid grid-cols-6 gap-2 w-full max-w-5xl">
+      {#each Array(6) as _, i}
+        <div
+          class="aspect-square w-full card shadow-sm flex items-center justify-center bg-red-200"
+        >
+          {#if selected[i] !== undefined}
+            <img
+              src={pokemon[selected[i]].sprite}
+              alt={pokemon[selected[i]].name}
+              class="max-h-full max-w-full object-contain"
+            />
+          {/if}
+        </div>
+      {/each}
+    </div>
+  </div>
 
-	<div class="w-1/2 mx-auto flex flex-col items-center">
-		<div class="grid grid-cols-5 gap-2 w-full mb-4">
-			{#each pokemon.slice((page-1) * perPage, page * perPage) as v, i}
-				<div
-					class="card w-full shadow-sm cursor-pointer hover:bg-red-200"
-					class:bg-red-200={selected.includes(i + (page-1)*perPage)}
-					onclick={() => handleClick(i + (page-1)*perPage)}
-				>
-					<figure>
-						<img src={v.sprite} alt={v.name}/>
-					</figure>
-				</div>
-			{/each}
-		</div>
+  <div class="w-1/2 mx-auto flex flex-col items-center">
+    <div
+      class="grid grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-2 w-full mb-4"
+    >
+      {#each pokemon.slice((page - 1) * perPage, page * perPage) as v, i}
+        <div
+          class="card w-full shadow-sm cursor-pointer hover:bg-red-200"
+          class:bg-red-200={selected.includes(i + (page - 1) * perPage)}
+          onclick={() => handleClick(i + (page - 1) * perPage)}
+        >
+          <figure>
+            <img src={v.sprite} alt={v.name} />
+          </figure>
+        </div>
+      {/each}
+    </div>
 
-		<div class="join sticky bottom-0 bg-base-100 w-full flex justify-center py-2">
-			<button class="join-item btn" onclick={prevPage}>«</button>
-			<button class="join-item btn">Page {page}</button>
-			<button class="join-item btn" onclick={nextPage}>»</button>
-		</div>
-	</div>
+    <div
+      class="join sticky bottom-0 bg-base-100 w-full flex justify-center py-2"
+    >
+      <button class="join-item btn" onclick={prevPage}>«</button>
+      <button class="join-item btn">Page {page}</button>
+      <button class="join-item btn" onclick={nextPage}>»</button>
+    </div>
+  </div>
 
-	<div class="w-1/2">
-		{#if selectedPokemon}
-			<div>
+  <div class="w-1/2 bg-yellow-100 rounded-xl">
+    {#if selectedPokemon}
+      <div>
+        <div class="card w-full shadow-sm">
+          <figure>
+            <img src={selectedPokemon.sprite} alt="Shoes" />
+          </figure>
 
-				<div class="card w-full shadow-sm">
-					<figure>
-						<img src={selectedPokemon.sprite} alt="Shoes"/>
-					</figure>
+          <div class="card-body">
+            <h2 class="card-title justify-center">
+              {toTitleCase(selectedPokemon.name)}
+            </h2>
 
-					<div class="card-body">
-						<h2 class="card-title justify-center">{toTitleCase(selectedPokemon.name)}</h2>
+            <div class="justify-center">
+              Typing:
+              {#each selectedPokemon.types as type}
+                {toTitleCase(type) + " "}
+              {/each}
+            </div>
 
-						<div class="justify-center"> Typing: 
-							{#each selectedPokemon.types as type}
-								{toTitleCase(type) + " "}
-							{/each}
-						</div>
-							
-						<p>
-							{#each selectedPokemon.stats as stat}
-								<div>{toTitleCase(stat.name)} : {stat.stat}</div>
-							{/each}
-						</p>
+            <p>
+              {#each selectedPokemon.stats as stat}
+                <div>
+                  {toTitleCase(stat.name)} : {stat.stat}
+                  <progress value={stat.stat} max="100" class="duration-2000" />
+                </div>
+              {/each}
+            </p>
 
+            <div class="justify-center">
+              Obtain:
+              {#if selectedPokemon.locations.find( (lo) => lo.versions.includes(game) )}
+                {toTitleCase(
+                  selectedPokemon.locations.find((lo) =>
+                    lo.versions.includes(game)
+                  ).location_area
+                )}
+              {:else}
+                Evolve
+              {/if}
+            </div>
+          </div>
 
-						<div class="justify-center">
-							Obtain: 
-							{#if selectedPokemon.locations.find(lo => lo.versions.includes(game))}
-								{toTitleCase(selectedPokemon.locations.find(lo => lo.versions.includes(game)).location_area)}
-							{:else}
-								Evolve
-							{/if}
-						</div>
-					</div>
-
-					<button class="btn btn-secondary" onclick={save}>Save Team</button>
-				</div>
-
-			</div>
-		{/if}
-	</div>
-	
+          <button class="btn btn-secondary" onclick={save}>Save Team</button>
+        </div>
+      </div>
+    {/if}
+  </div>
 </div>
