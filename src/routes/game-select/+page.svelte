39e8f0b0d0834e8e9745ca20c6c5054ef24d03cf
MarcusChang0 <changm2608@gmail.com>
2025-10-25 16:53:47 -0700

    please god

--- src/routes/game-select/+page.svelte
+++ src/routes/game-select/+page.svelte
@@ -11,70 +11,15 @@
   </div>
 </div>
 
-<div class="join join-vertical p-6 flex flex-col-reverse">
-  <div class="collapse collapse-arrow join-item border-base-300 border">
-    <input type="radio" name="my-accordion-4" />
-    <div class="collapse-title font-semibold">Generation 9</div>
-    <div class="collapse-content text-sm px-12">
-      <li><a href="/scarlet">Pokemon Scarlet</a></li>
-      <li><a href="/violet">Pokemon Violet</a></li>
-    </div>
-  </div>
+<div class="join join-vertical p-6 flex flex-col">
 
   <div class="collapse collapse-arrow join-item border-base-300 border">
     <input type="radio" name="my-accordion-4" />
-    <div class="collapse-title font-semibold">Generation 8</div>
-    <div class="collapse-content text-sm px-12 px-12">
-      <li><a href="/sword">Pokemon Sword</a></li>
-      <li><a href="/shield">Pokemon Shield</a></li>
-    </div>
-  </div>
-
-  <div class="collapse collapse-arrow join-item border-base-300 border">
-    <input type="radio" name="my-accordion-4" />
-    <div class="collapse-title font-semibold">Generation 7</div>
+    <div class="collapse-title font-semibold">Generation 1</div>
     <div class="collapse-content text-sm px-12">
-      <li><a href="/sun">Pokemon Sun</a></li>
-      <li><a href="/moon">Pokemon Moon</a></li>
-    </div>
-  </div>
-
-  <div class="collapse collapse-arrow join-item border-base-300 border">
-    <input type="radio" name="my-accordion-4" />
-    <div class="collapse-title font-semibold">Generation 6</div>
-    <div class="collapse-content text-sm px-12">
-      <li><a href="/x">Pokemon X</a></li>
-      <li><a href="/y">Pokemon Y</a></li>
-    </div>
-  </div>
-
-  <div class="collapse collapse-arrow join-item border-base-300 border">
-    <input type="radio" name="my-accordion-4" />
-    <div class="collapse-title font-semibold">Generation 5</div>
-    <div class="collapse-content text-sm px-12">
-      <li><a href="/diamond">Pokemon Black</a></li>
-      <li><a href="/pearl">Pokemon White</a></li>
-    </div>
-  </div>
-
-  <div class="collapse collapse-arrow join-item border-base-300 border">
-    <input type="radio" name="my-accordion-4" />
-    <div class="collapse-title font-semibold">Generation 4</div>
-    <div class="collapse-content text-sm px-12">
-      <li><a href="/diamond">Pokemon Diamond</a></li>
-      <li><a href="/pearl">Pokemon Pearl</a></li>
-    </div>
-  </div>
-
-  <div class="collapse collapse-arrow join-item border-base-300 border">
-    <input type="radio" name="my-accordion-4" />
-    <div class="collapse-title font-semibold">Generation 3</div>
-    <div class="collapse-content text-sm px-12">
-      <li><a href="/ruby">Pokemon Ruby</a></li>
-      <li><a href="/sapphire">Pokemon Sapphire</a></li>
-      <li><a href="/emerald">Pokemon Emerald</a></li>
-      <li><a href="/firered">Pokemon FireRed</a></li>
-      <li><a href="/leafgreen">Pokemon LeafGreen</a></li>
+      <li><a href="/game/red">Pokémon Red</a></li>
+      <li><a href="/game/blue">Pokémon Blue</a></li>
+      <li><a href="/game/yellow">Pokémon Yellow</a></li>
     </div>
   </div>
 
@@ -82,17 +27,90 @@
     <input type="radio" name="my-accordion-4" />
     <div class="collapse-title font-semibold">Generation 2</div>
     <div class="collapse-content text-sm px-12">
-      <li><a href="/gold">Pokemon Gold</a></li>
-      <li><a href="/silver">Pokemon Silver</a></li>
+      <li><a href="/game/gold">Pokémon Gold</a></li>
+      <li><a href="/game/silver">Pokémon Silver</a></li>
+      <li><a href="/game/crystal">Pokémon Crystal</a></li>
     </div>
   </div>
 
   <div class="collapse collapse-arrow join-item border-base-300 border">
     <input type="radio" name="my-accordion-4" />
-    <div class="collapse-title font-semibold">Generation 1</div>
+    <div class="collapse-title font-semibold">Generation 3</div>
     <div class="collapse-content text-sm px-12">
-      <li><a href="/game/red">Pokemon Red</a></li>
-      <li><a href="/blue">Pokemon Blue</a></li>
+      <li><a href="/game/ruby">Pokémon Ruby</a></li>
+      <li><a href="/game/sapphire">Pokémon Sapphire</a></li>
+      <li><a href="/game/emerald">Pokémon Emerald</a></li>
+      <li><a href="/game/firered">Pokémon FireRed</a></li>
+      <li><a href="/game/leafgreen">Pokémon LeafGreen</a></li>
+    </div>
+  </div>
+
+  <div class="collapse collapse-arrow join-item border-base-300 border">
+    <input type="radio" name="my-accordion-4" />
+    <div class="collapse-title font-semibold">Generation 4</div>
+    <div class="collapse-content text-sm px-12">
+      <li><a href="/game/diamond">Pokémon Diamond</a></li>
+      <li><a href="/game/pearl">Pokémon Pearl</a></li>
+      <li><a href="/game/platinum">Pokémon Platinum</a></li>
+      <li><a href="/game/heartgold">Pokémon HeartGold</a></li>
+      <li><a href="/game/soulsilver">Pokémon SoulSilver</a></li>
+    </div>
+  </div>
+
+  <div class="collapse collapse-arrow join-item border-base-300 border">
+    <input type="radio" name="my-accordion-4" />
+    <div class="collapse-title font-semibold">Generation 5</div>
+    <div class="collapse-content text-sm px-12">
+      <li><a href="/game/black">Pokémon Black</a></li>
+      <li><a href="/game/white">Pokémon White</a></li>
+      <li><a href="/game/black-2">Pokémon Black 2</a></li>
+      <li><a href="/game/white-2">Pokémon White 2</a></li>
+    </div>
+  </div>
+
+  <div class="collapse collapse-arrow join-item border-base-300 border">
+    <input type="radio" name="my-accordion-4" />
+    <div class="collapse-title font-semibold">Generation 6</div>
+    <div class="collapse-content text-sm px-12">
+      <li><a href="/x">Pokémon X</a></li>
+      <li><a href="/y">Pokémon Y</a></li>
+      <li><a href="/x">Pokémon Omega Ruby</a></li>
+      <li><a href="/y">Pokémon Alpha Sapphire</a></li>
+    </div>
+  </div>
+
+  <div class="collapse collapse-arrow join-item border-base-300 border">
+    <input type="radio" name="my-accordion-4" />
+    <div class="collapse-title font-semibold">Generation 7</div>
+    <div class="collapse-content text-sm px-12">
+      <li><a href="/sun">Pokémon Sun</a></li>
+      <li><a href="/moon">Pokémon Moon</a></li>
+      <li><a href="/sun">Pokémon Ultra Sun</a></li>
+      <li><a href="/moon">Pokémon Ultra Moon</a></li>
+      <li><a href="/sun">Pokémon Let's Go Pikachu!</a></li>
+      <li><a href="/moon">Pokémon Let's go Eevee!</a></li>
+    </div>
+  </div>
+
+  <div class="collapse collapse-arrow join-item border-base-300 border">
+    <input type="radio" name="my-accordion-4" />
+    <div class="collapse-title font-semibold">Generation 8</div>
+    <div class="collapse-content text-sm px-12 px-12">
+      <li><a href="/sword">Pokémon Sword</a></li>
+      <li><a href="/shield">Pokémon Shield</a></li>
+      <li><a href="/sword">Pokémon Brilliant Diamond</a></li>
+      <li><a href="/shield">Pokémon Shining Pearl</a></li>
+      <li><a href="/sword">Pokémon Legends Arceus</a></li>
+    </div>
+  </div>
+
+  <div class="collapse collapse-arrow join-item border-base-300 border">
+    <input type="radio" name="my-accordion-4" />
+    <div class="collapse-title font-semibold">Generation 9</div>
+    <div class="collapse-content text-sm px-12">
+      <li><a href="/scarlet">Pokémon Scarlet</a></li>
+      <li><a href="/violet">Pokémon Violet</a></li>
+      <li><a href="/violet">Pokémon Legends ZA</a></li>
     </div>
   </div>
 </div>
